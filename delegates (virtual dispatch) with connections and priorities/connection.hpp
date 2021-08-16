561aa7caef61ee71dc33b4362ecac3d786b5d230
Luca Davidian <lucadavidian@gmail.com.github.com>
2021-08-16 11:51:49 +0200

    Connection has default constructor

--- delegates (virtual dispatch) with connections and priorities/connection.hpp
+++ delegates (virtual dispatch) with connections and priorities/connection.hpp
@@ -10,8 +10,11 @@ class CallableWrapper;
 class Connection
 {
 public:
+    Connection() : mSignal(nullptr) {}
+    
     template <typename Ret, typename... Args>
     Connection(Signal<Ret(Args...)> *signal, CallableWrapper<Ret(Args...)> *callableWrapper) : mSignal(signal), mCallableWrapper(callableWrapper), mDisconnectFunction(&DisconnectFunction<Ret, Args...>) {}
+    
     void Disconnect() { mDisconnectFunction(mSignal, mCallableWrapper); }
 private:
     void (*mDisconnectFunction)(void*, void*);
@@ -21,7 +24,8 @@ private:
     template <typename Ret, typename... Args>
     static void DisconnectFunction(void *signal, void *callableWrapper)
     {
-        static_cast<Signal<Ret(Args...)>*>(signal)->Unbind(static_cast<CallableWrapper<Ret(Args...)>*>(callableWrapper));
+        if (signal)
+            static_cast<Signal<Ret(Args...)>*>(signal)->Unbind(static_cast<CallableWrapper<Ret(Args...)>*>(callableWrapper));
     }
 };
 
